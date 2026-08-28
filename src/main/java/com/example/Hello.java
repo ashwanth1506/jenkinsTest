0f267214ea2a23640980dcaa99951f4d31b42516
unknown <you@example.com>
2026-08-28 12:54:51 +0530

    testing

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven + Jenkins!");
+        System.out.println("Hello from Maven + Jenkins poll!");
     }
 
     public static String message() {
